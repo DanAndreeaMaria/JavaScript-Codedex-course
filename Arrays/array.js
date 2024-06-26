cce133c123e56e6b08ee88b25a93e58325bef120
Andreea <106435918+DanAndreeaMaria@users.noreply.github.com>
2024-06-26 14:51:47 +0300

    Add files via upload

---
+++ Arrays/array.js
@@ -0,0 +1,9 @@
+let groceryList = ["Milk", " Avocado", " Eggs", " Bread"];
+
+console.log("From: " + groceryList);
+
+groceryList [2] = " Butter";
+
+groceryList [4] = " Laundry soap";
+
+console.log("To this: " + groceryList);
