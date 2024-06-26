f4e752e860798fdb0ace478cf340cb62ec9dbe69
Andreea <106435918+DanAndreeaMaria@users.noreply.github.com>
2024-06-26 14:53:29 +0300

    Add files via upload

---
+++ BMI/bmi.js
@@ -0,0 +1,11 @@
+let mass = 65;
+
+console.log("Mass: " + mass + " kg");
+
+let height = 1.70;
+
+console.log("Height: " + height + " m");
+
+let bmi = Math.floor(mass / height ** 2);
+
+console.log("Your BMI is " + bmi + ".");
