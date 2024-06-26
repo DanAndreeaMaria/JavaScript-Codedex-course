9f7ff0a3429a8c045609506c64283801f3937d6e
Andreea <106435918+DanAndreeaMaria@users.noreply.github.com>
2024-06-26 15:03:32 +0300

    Add files via upload

---
+++ Variables/let_and_const.js
@@ -0,0 +1,11 @@
+let firstName = "Andreea";
+const year = 1995;
+let age = 28;
+const month = "November";
+let city = "Deva";
+
+// console.log(firstName + " is born on year " + year + ", " + month + " month, in " + city + " city and has the age of " + age + " years old.")
+
+city = "Cluj";
+
+console.log(firstName + " is born on year " + year + ", " + month + " month, she might think about moving to " + city + " city and has the age of " + age + " years old.")
