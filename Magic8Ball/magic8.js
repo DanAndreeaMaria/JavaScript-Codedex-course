6e780ecb77f17f21dea816f75a0036b714f935a6
Andreea <106435918+DanAndreeaMaria@users.noreply.github.com>
2024-06-26 14:59:13 +0300

    Add files via upload

---
+++ Magic8Ball/magic8.js
@@ -0,0 +1,27 @@
+let question = ("Question: Is Codédex better than Udemy yet?");
+
+console.log(question);
+
+let magicBall = Math.floor((Math.random() * 10) - 1);
+
+if (magicBall === 1) {
+    console.log("Magic 8 Ball: Yes - definitely.");
+  } else if (magicBall === 2) {
+    console.log("Magic 8 Ball: It is decidedly so.")
+  } else if(magicBall === 3) {
+    console.log("Magic 8 Ball: Without a doubt.")
+  } else if(magicBall === 4) {
+    console.log("Magic 8 Ball: Reply hazy, try again.")
+  } else if(magicBall === 5) {
+    console.log("Magic 8 Ball: Ask again later.")
+  } else if(magicBall === 6) {
+    console.log("Magic 8 Ball: Better not tell you now.")
+  } else if(magicBall === 7) {
+    console.log("Magic 8 Ball: My sources say no.")
+  } else if(magicBall === 8) {
+    console.log("Magic 8 Ball: Outlook not so good.")
+  } else if(magicBall === 9) {
+    console.log("Magic 8 Ball: Outlook not so good.")
+  } else {
+    console.log("Null")
+  }
