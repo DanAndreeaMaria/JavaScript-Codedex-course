193404439d78fbdc0edd2d0b81df4e0b6b84ada4
Andreea <106435918+DanAndreeaMaria@users.noreply.github.com>
2024-06-26 15:02:56 +0300

    Add files via upload

---
+++ TimesTable/timestables.js
@@ -0,0 +1,8 @@
+let multiple = 5;
+
+let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+for (let i = 0; i < numbers.length; i++) {
+    let result = multiple * numbers[i];
+    console.log(multiple + " x " + numbers[i] + " = " + result);
+}
